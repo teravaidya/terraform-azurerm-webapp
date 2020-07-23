cad10c203d382ead35cebc33573ac4c5f3cc42ee
Mikael <krief_mikael@hotmail.com>
2020-07-23 14:09:20 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "azurerm_app_service_plan" "plan-app" {
   location            = var.location
   resource_group_name = var.resource_groupe_name
   sku {
-    tier = "Standard"
+    tier = var.sp_sku
     size = "S1"
   }
 }
